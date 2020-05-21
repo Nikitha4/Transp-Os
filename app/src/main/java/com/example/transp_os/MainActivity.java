3deb8c5a7fc902793d9e1e0d6f1f97bdee698c3b
Nikitha Nagumalli <nikitha4@illinois.edu>
2020-05-21 18:43:32 -0500

    added jar file, still need to get access to source code

--- app/src/main/java/com/example/transp_os/MainActivity.java
+++ app/src/main/java/com/example/transp_os/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.transp_os;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import org.jfugue.player.Player;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public static void main(String[] args) {
+        Player player = new Player();
+        player.play("C D E F G A B");
     }
 }
